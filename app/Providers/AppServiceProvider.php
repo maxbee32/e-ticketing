0c318027d211ad3f8a6e747b611919ec9cd3a8c3
Maxwell <mboakye@excenit.com>
2023-02-17 22:10:16 +0000

    removed ngrok setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,15 +23,15 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot(\Illuminate\Http\Request $request)
-    // {
-    //     Schema::defaultStringLength(191);
-    // }
-
     {
-        if (!empty( env('NGROK_URL') ) && $request->server->has('HTTP_X_ORIGINAL_HOST')) {
-            $this->app['url']->forceRootUrl(env('NGROK_URL'));
-        }
+        Schema::defaultStringLength(191);
     }
+
+    // {
+    //     if (!empty( env('NGROK_URL') ) && $request->server->has('HTTP_X_ORIGINAL_HOST')) {
+    //         $this->app['url']->forceRootUrl(env('NGROK_URL'));
+    //     }
+    // }
 
 
 
